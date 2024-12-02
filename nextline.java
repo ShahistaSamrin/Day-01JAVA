5207aee46432cbb85a4af2b2b2a80df3205e1585
ShahistaSamrin <shaikshahistasamrin@gmail.com>
2024-12-02 15:50:11 +0530

    Add files via upload

---
+++ nextline.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class nextline{
+    public static void main(String args[]){
+        //accepting a String
+        Scanner sc=new Scanner(System.in);
+        String a=sc.nextline();
+        String b=sc.nextline();
+        String c=sc.nextline();
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
