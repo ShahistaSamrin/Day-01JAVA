35505ad05b757b86e303e70691aaa003677e9900
ShahistaSamrin <shaikshahistasamrin@gmail.com>
2024-12-02 15:12:54 +0530

    Add files via upload

---
+++ typeconversion.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class typeconversion{
+    public static void main(String args[])
+    {
+    //conversion of ascii value to character
+    int a=50;
+    char b=(char)(a);
+    char c='7';
+    System.out.println(b);
+    System.out.println(c);
+    }   
+}
