dcdce5fb8f4b79f94b6f46a5261050d7c4e68999
ShahistaSamrin <shaikshahistasamrin@gmail.com>
2024-12-02 16:15:30 +0530

    Add files via upload

---
+++ acceptstring.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class acceptstring{
+    public static void main(String args[]){
+        //accepting a String
+        Scanner sc=new Scanner(System.in);
+        String a=sc.next();
+        System.out.println("congratulations" +a +" you won 5 lakhs");
+        }
+}
