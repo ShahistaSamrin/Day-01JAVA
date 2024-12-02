28f5561efea81f1121f5b75c468e505328d11e28
ShahistaSamrin <shaikshahistasamrin@gmail.com>
2024-12-02 15:23:08 +0530

    Add files via upload

---
+++ printingnextchar.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class printingnextchar{
+    public static void main(String args[]){
+        char c='a';
+        int b=c;
+        char v=(char)b;
+        b+=1;
+        System.out.println(v);
+    }
+}
